8e5e02060989e400c29b803b3ce474cd27611b8c
RichardDev01 <60653249+RichardDev01@users.noreply.github.com>
2021-02-13 23:33:33 +0100

    Added time comparison and new Threading method

--- Python code/opdracht3.5.py
+++ Python code/opdracht3.5.py
@@ -5,6 +5,7 @@
 #
 import threading
 import concurrent.futures
+from timeit import default_timer as timer
 
 
 def f(x):
@@ -36,6 +37,48 @@ class WorkerThread(threading.Thread):
         self.n2 += f(i * self.deltaX)
         # self._lock.release()
 
+    def get(self):
+        """
+        gets the computed sum.
+        """
+        return self.n2 * self.deltaX
+
+
+class WorkerThreadRework(threading.Thread):
+
+    def __init__(self, args):
+        """
+        args must contain n1, n2, and deltaX, in that order.
+        """
+        threading.Thread.__init__(self, args=args)
+        self.n1 = args[0]  # N
+        self.n2 = args[1]  # Sum
+        self.deltaX = args[2]
+
+    def run(self):
+        """
+        This will be called automatically by start() in
+        main.  It's the method that does all the work.
+        """
+
+        start = timer()
+        threads = list()
+        for index in range(self.n1):
+            x = threading.Thread(target=self.compute_function, args=(index,))
+            threads.append(x)
+            x.start()
+
+        for x in threads:
+            x.join()
+
+        # display results
+
+        elapsed_time = timer() - start
+
+        print(f"Threading attempt #2\n sum = {self.get()} \n elapsed_time time {elapsed_time}\n")
+
+    def compute_function(self, index):
+        self.n2 += f(index * self.deltaX)
 
     def get(self):
         """
@@ -52,20 +95,38 @@ def main():
     """
 
     # get the number of terms
-    N = int(input("> "))
+    # N = int(input("> "))
+    N = 3
 
     sum = 0.0  # where we sum up the individual
     # intervals
     deltaX = 1.0 / N  # the interval
 
+    # ------------------------------------------------------------
+    start = timer()
     workers = WorkerThread(args=[sum, deltaX])
 
     with concurrent.futures.ThreadPoolExecutor(max_workers=N) as executor:
         for i in range(N):
             executor.submit(workers.run(i))
 
+    elapsed_time = timer() - start
+    print(f"Threading attempt #1\n sum = {workers.get()} \n elapsed_time time {elapsed_time}\n")
 
-    # display results
-    print("sum = %1.9f" % (workers.get()))
+    # ------------------------------------------------------------
+
+    workersRework = WorkerThreadRework(args=[N, sum, deltaX])
+
+    workersRework.start()
+
+    # ------------------------------------------------------------
+    start = timer()
+    # sum over N terms
+    for i in range(N):
+        sum += f(i * deltaX)
+
+    elapsed_time = timer() - start
+    print(f"No threading \n sum = {(sum * deltaX)}\n elapsed_time time {elapsed_time}\n")
+
 
 main()
